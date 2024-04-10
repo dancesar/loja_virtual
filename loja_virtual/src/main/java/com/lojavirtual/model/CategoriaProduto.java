48c0d7f24230570b7ddf54536accb2d02d7c99ab
dancesar <danillo.yamagush@gmail.com>
2024-04-10 11:02:29 -0300

    Criada classe CategoriaProduto.

---
+++ loja_virtual/src/main/java/com/lojavirtual/model/CategoriaProduto.java
@@ -0,0 +1,22 @@
+package com.lojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+@Data
+public class CategoriaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+    private Long id;
+
+    @Column(name = "nome_descricao", nullable = false)
+    private String nomeDesc;
+}
