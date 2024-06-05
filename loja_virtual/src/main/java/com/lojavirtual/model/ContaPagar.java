bbb49a9cc3cad6927cf7d0b20f1f14e0d2d3eaa1
Danillo <danillo.yamagush@gmail.com>
2024-06-05 04:14:49 -0300

    Atualizado campos que não estavam not null no banco de dados

--- loja_virtual/src/main/java/com/lojavirtual/model/ContaPagar.java
+++ loja_virtual/src/main/java/com/lojavirtual/model/ContaPagar.java
@@ -18,17 +18,21 @@ public class ContaPagar implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
     private Long id;
 
+    @Column(nullable = false)
     private String descricao;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private StatusContaPagar status;
 
+    @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private Date dtVencimento;
 
     @Temporal(TemporalType.DATE)
     private Date dtPagamento;
 
+    @Column(nullable = false)
     private BigDecimal valorTotal;
 
     private BigDecimal valorDesconto;
--- loja_virtual/src/main/java/com/lojavirtual/model/NotaFiscalCompra.java
+++ loja_virtual/src/main/java/com/lojavirtual/model/NotaFiscalCompra.java
@@ -19,12 +19,20 @@ public class NotaFiscalCompra implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
     private Long id;
 
+    @Column(nullable = false)
     private String numeroNota;
+
+    @Column(nullable = false)
     private String serieNota;
+
+    @Column(nullable = false)
     private BigDecimal valorTotal;
     private BigDecimal valorDesconto;
+
+    @Column(nullable = false)
     private BigDecimal valorIcms;
 
+    @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private Date dataCompra;
 
