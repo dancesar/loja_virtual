150dc7b490381be590c881456e10b413a58ab166
dancesar <danillo.yamagush@gmail.com>
2024-06-08 03:14:03 -0300

    Criado primeiros testes da aplicação no Acesso.

--- loja_virtual/src/main/java/com/lojavirtual/controller/AcessoController.java
+++ loja_virtual/src/main/java/com/lojavirtual/controller/AcessoController.java
@@ -1,10 +1,15 @@
 package com.lojavirtual.controller;
 
 import com.lojavirtual.model.Acesso;
+import com.lojavirtual.repository.AcessoRepository;
 import com.lojavirtual.service.AcessoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AcessoController {
@@ -12,8 +17,22 @@ public class AcessoController {
     @Autowired
     private AcessoService acessoService;
 
-    @PostMapping("/salvaAcesso")
-    public Acesso salvarAcesso(Acesso acesso) {
-        return acessoService.save(acesso);
+    @Autowired
+    private AcessoRepository acessoRepository;
+
+    @ResponseBody
+    @PostMapping(value = "/salvarAcesso")
+    public ResponseEntity<Acesso>  salvarAcesso(@RequestBody Acesso acesso) {
+        Acesso acessoSalvo = acessoService.save(acesso);
+
+        return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/deleteAcesso")
+    public ResponseEntity<?>  deleteAcesso(@RequestBody Acesso acesso) {
+        acessoRepository.deleteById(acesso.getId());
+
+        return new ResponseEntity("Acesso removido", HttpStatus.OK);
     }
 }
--- loja_virtual/src/test/java/com/lojavirtual/LojaVirtualApplicationTests.java
+++ loja_virtual/src/test/java/com/lojavirtual/LojaVirtualApplicationTests.java
@@ -3,22 +3,24 @@ package com.lojavirtual;
 import com.lojavirtual.controller.AcessoController;
 import com.lojavirtual.model.Acesso;
 import com.lojavirtual.repository.AcessoRepository;
-import com.lojavirtual.service.AcessoService;
+import junit.framework.TestCase;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = LojaVirtualApplication.class)
-class LojaVirtualApplicationTests {
-
-	@Autowired
-	private AcessoService acessoService;
-
-	@Autowired
-	private AcessoRepository acessoRepository;
+public class LojaVirtualApplicationTests extends TestCase {
 
 	@Autowired
 	private AcessoController acessoController;
+
+	@Autowired
+	private AcessoRepository acessoRepository;
 
 	@Test
 	public void testCadastraAcesso() {
@@ -26,7 +28,31 @@ class LojaVirtualApplicationTests {
 
 		acesso.setDescricao("ROLE_ADMIN");
 
-		acessoController.salvarAcesso(acesso);
-	}
+		assertEquals(true, acesso.getId() == null);
 
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+
+		assertEquals(true, acesso.getId() > 0);
+
+		assertEquals("ROLE_ADMIN", acesso.getDescricao());
+
+		//Teste de carregamento
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+
+		assertEquals(acesso.getId(), acesso2.getId());
+
+		//teste de delete
+		acessoRepository.deleteById(acesso2.getId());
+		acessoRepository.flush(); /*Roda esse SQL de delete no banco de dados*/
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		assertEquals(true, acesso3 == null);
+
+		/*Teste de Query*/
+		acesso = new Acesso();
+		acesso.setDescricao("ROLE_ALUNO");
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+		assertEquals(1, acessos.size());
+		acessoRepository.deleteById(acesso.getId());
+	}
 }
